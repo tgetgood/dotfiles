6bfb288accf5b201e362115a8b42c949773a6dfd
Thomas Getgood <thomas@evolvingweb.ca>
2011-08-12 17:56:17 -0400

    Overhauled setup script to backup and erase dotfiles that already exist (doesn't back up symlinks). Makes life easier for me at least.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,23 @@
+# Simple script to copy dotfiles to home directory.
+# If the files already exist, back them up and delete them.
+# Does not back up symlinks.
+# overwirtes .file~ if it exists.
+
+# Originally copied from Alex's dotfiles, but not much remains
+# last edited by Thomas on 12/08/2011
+
 SCRIPT_PATH=$(readlink -f $(dirname "${VIRTUAL_ENV}"))
-ln -s $SCRIPT_PATH/vimrc ~/.vimrc
-ln -s $SCRIPT_PATH/vim ~/.vim
-ln -s $SCRIPT_PATH/bashrc ~/.bashrc
-ln -s $SCRIPT_PATH/gitconfig ~/.gitconfig
-ln -s $SCRIPT_PATH/my.cnf ~/.my.cnf
+FILES="vimrc vim bashrc my.cnf gitconfig"
+
+for f in $FILES
+do
+  # if file exists, back it up
+  if [ -e ~/."$f" ]
+  then
+    [ -s ~/."$f" ] && rm ~/."$f" || mv ~/."$f" ~/."$f"'~'
+  fi
+
+  ln -s $SCRIPT_PATH/$f ~/."$f"
+
+done
+
