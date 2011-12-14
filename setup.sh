e37d38f37d457733ba95d11912d2f2783718a3b2
Thomas Getgood <thomas@catnip.ewdev.ca>
2011-12-14 10:44:32 -0500

    Modified for github.

--- setup.sh
+++ setup.sh
@@ -3,24 +3,23 @@
 # Does not back up symlinks.
 # overwirtes .file~ if it exists.
 
-# Originally copied from Alex's dotfiles, but not much remains
-# last edited by Thomas on 12/08/2011
+# last edited by on 14/12/2011
 
-HOME=/home/thomas
+HOME=~thomas
 
-SCRIPT_PATH=$(readlink -f $(dirname "${VIRTUAL_ENV}"))
+SCRIPT_PATH=$PWD
 # This is not very good in general, just so far. 
-FILES=`ls | grep -v '\.sh$'`
+FILES=`ls -a | grep -v 'setup\.sh|\.git'`
 
 for f in $FILES
 do
   # if file exists and is not a symlink, back it up
-  if [ -e $HOME/."$f" ]
+  if [ -e $HOME/"$f" ]
   then
-    [ -h $HOME/."$f" ] && rm $HOME/."$f" || mv $HOME/."$f" $HOME/."$f"'~'
+    [ -h "$HOME/$f" ] && rm "$HOME/$f" || mv "$HOME/$f" "$HOME/$f~"
   fi
 
-  ln -s $SCRIPT_PATH/"$f" $HOME/."$f"
+  ln -s "$SCRIPT_PATH/$f" "$HOME/$f"
 
 done
 
