9b2d524a558728dc63f12656b6c0efbb28e3007e
Thomas Getgood <thomas.getgood@gmail.com>
2014-12-22 15:08:03 -0500

    Magit has nice keybindings of its own.

--- .emacs.d/config/evil-setup.el
+++ .emacs.d/config/evil-setup.el
@@ -15,15 +15,6 @@
 
 (define-key key-translation-map (kbd "C-g") 'esc-non-normal)
 
-(defvar my-emacs-modes
-	'(
-		cider-docview-mode
-		cider-stacktrace-mode
-		haskell-error-mode
-		haskell-interactive-mode
-		
-		))
-
 (evil-mode 1)
 
 ;; Handy helper to figure out buffer mode names for below list. 
@@ -32,8 +23,38 @@
   (with-current-buffer buffer-or-string
      major-mode))
 
+;;;;;
+;; Exceptions to Evil
+;;;;;
+
+(defvar my-emacs-modes
+	'(
+		cider-docview-mode
+		cider-stacktrace-mode
+
+		haskell-error-mode
+		haskell-interactive-mode
+
+		dired-mode
+
+		magit-auto-revert-mode			
+		magit-blame-mode						
+		magit-branch-manager-mode		
+		magit-cherry-mode						
+		magit-commit-mode						
+		magit-diff-mode							
+		magit-key-mode							
+		magit-log-edit-mode					
+		magit-log-mode							
+		magit-mode									
+		magit-process-mode					
+		magit-reflog-mode						
+		magit-status-mode						
+		magit-wazzup-mode						
+		magit-wip-save-mode					
+
+		))
+
 (dolist (m my-emacs-modes) 
 	(add-to-list 'evil-emacs-state-modes m))
 
-;; Evil isn't always appropriate... Or is it?
-;; (add-to-list 'evil-emacs-state-modes ...)
