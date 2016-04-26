551c8f4497cb21f16a487d3867707851ad4ff427
Thomas Getgood <thomas.getgood@gmail.com>
2016-04-26 08:57:58 -0400

    Whitespace cleanup.

--- .emacs.d/config/evil-setup.el
+++ .emacs.d/config/evil-setup.el
@@ -18,11 +18,11 @@
 (global-evil-leader-mode)
 (evil-mode 1)
 
-;; Handy helper to figure out buffer mode names for below list. 
+;; Handy helper to figure out buffer mode names for below list.
 (defun buffer-mode (buffer-or-string)
-  "Returns the major mode associated with a buffer."
-  (with-current-buffer buffer-or-string
-     major-mode))
+	"Returns the major mode associated with a buffer."
+	(with-current-buffer buffer-or-string
+		 major-mode))
 
 ;;;;;
 ;; Exceptions to Evil
@@ -34,31 +34,31 @@
 		cider-stacktrace-mode
 		cider-test-report-mode
 		cider-repl-mode
-		
+
 		haskell-error-mode
 		haskell-interactive-mode
 
 		dired-mode
 
-		magit-auto-revert-mode			
-		magit-blame-mode						
-		magit-branch-manager-mode		
-		magit-cherry-mode						
-		magit-commit-mode						
-		magit-diff-mode							
-		magit-key-mode							
-		magit-log-edit-mode					
-		magit-log-mode							
-		magit-mode									
-		magit-process-mode					
-		magit-reflog-mode						
-		magit-status-mode						
-		magit-wazzup-mode						
-		magit-wip-save-mode					
+		magit-auto-revert-mode
+		magit-blame-mode
+		magit-branch-manager-mode
+		magit-cherry-mode
+		magit-commit-mode
+		magit-diff-mode
+		magit-key-mode
+		magit-log-edit-mode
+		magit-log-mode
+		magit-mode
+		magit-process-mode
+		magit-reflog-mode
+		magit-status-mode
+		magit-wazzup-mode
+		magit-wip-save-mode
 
 		))
 
-(dolist (m my-emacs-modes) 
+(dolist (m my-emacs-modes)
 	(add-to-list 'evil-emacs-state-modes m))
 
 (define-key evil-normal-state-map (kbd "M-.") nil)
@@ -69,41 +69,44 @@
 
 (evil-leader/set-key
 
-	;;;;; Clojure bindings
+	;; Clojure bindings
 	"r" 'cider-eval-region
 	"E" 'cider-load-buffer
 
- ;;;;; Kibit
+	;; Kibit
  "k" 'kibit-accept-proposed-change
  "K" 'kibit-current-file
 
- ;;;; clj refactor
+ ;; clj refactor
  "c" 'cljr-clean-ns
  "m" 'cljr-add-missing-libspec
- 
-	;;;;; compilation error list
+
+ ;; compilation error list
  "[" 'previous-error
  "]" 'next-error
 
- ;;;;; indentation helpers
+ ;; indentation helpers
  "TAB" (lambda () (interactive)
 				 (if (region-active-p)
 						 (evil-indent (region-beginning) (region-end))
-					   (evil-indent-line (point) (1+ (point)))))
+						 (evil-indent-line (point) (1+ (point)))))
 
  "e" 'eval-buffer
  "a" 'easy-ack
 
- ;;;;; Magit
+ ;; Magit
  "s" 'magit-status
  "b" 'magit-blame
  "B" 'magit-blame-mode
 
- ;;;;; Ghetto tasklist pluging
+ ;; Ghetto tasklist pluging
  "t" 'tasklist-ack ; Defined in global.el
  "q" (lambda () (interactive)
 			 (progn (switch-to-buffer-other-window "*Ack-and-a-half*")
 							(quit-window)))
+ ;; Everyday stuff
+ "w" 'whitespace-cleanup
+
  )
 
 (evil-ex-define-cmd "E[xplore]" (lambda () (interactive) (dired ".")))
