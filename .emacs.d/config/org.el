4a98698840e3b1c072f72ab6ab9bb8a03cff9c48
Thomas Getgood <thomas.getgood@gmail.com>
2016-10-21 21:13:59 -0400

    More org config.

--- .emacs.d/config/org.el
+++ .emacs.d/config/org.el
@@ -1,6 +1,17 @@
-(setq org-log-done t)
-(setq org-log-repeat 'time)
+;; Don't clobber windows
+(setq org-agenda-window-setup 'current-window)
+
+;; Agenda
 (setq org-agenda-files '("~/org"))
 
+;; Time Tracking
 (setq org-clock-persist 'history)
 (org-clock-persistence-insinuate)
+
+;; TODO config
+(setq-default org-highest-priority ?A)
+(setq-default org-lowest-priority ?E)
+(setq-default org-default-priority ?D)
+
+(setq org-log-done t)
+(setq org-log-repeat 'time)
