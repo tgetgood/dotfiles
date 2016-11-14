fd481f0718d25290878175a1b4e7a57562c12651
Thomas Getgood <thomas.getgood@gmail.com>
2016-11-13 19:14:48 -0500

    Version upgrades.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -139,6 +139,9 @@
  '(org-modules
 	 (quote
 		(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(package-selected-packages
+	 (quote
+		(cider wanderlust tagedit smex scss-mode scion popup meacupla-theme markdown-mode magit kibit-mode kibit-helper jabber ido-ubiquitous haskell-mode gh flycheck-pos-tip evil-smartparens evil-paredit evil-leader cyberpunk-theme company clojure-mode-extra-font-locking cljsbuild-mode clj-refactor bash-completion ag ack-and-a-half)))
  '(sp-autoskip-closing-pair t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
