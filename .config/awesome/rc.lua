eca0bed85243e4422b7b82071904b6cf319cafc0
Thomas Getgood <thomas.getgood@gmail.com>
2017-02-04 17:43:48 -0500

    Layouts.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,4 +1,4 @@
-lname-- @DOC_REQUIRE_SECTION@
+-- @DOC_REQUIRE_SECTION@
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
@@ -73,19 +73,19 @@ awful.layout.layouts = {
     awful.layout.suit.floating,
   --awful.layout.suit.tile,
   --awful.layout.suit.tile.left,
+    awful.layout.suit.fair,
     awful.layout.suit.tile.bottom,
   --awful.layout.suit.tile.top,
-  awful.layout.suit.fair,
   --awful.layout.suit.fair.horizontal,
   --awful.layout.suit.spiral,
   --awful.layout.suit.spiral.dwindle,
   --awful.layout.suit.max,
-  awful.layout.suit.max.fullscreen,
+    awful.layout.suit.max.fullscreen,
   --awful.layout.suit.magnifier,
   --awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
+  --awful.layout.suit.corner.ne,
+  --awful.layout.suit.corner.sw,
+  --awful.layout.suit.corner.se,
 }
 
 awful.util.taglist_buttons = awful.util.table.join(
