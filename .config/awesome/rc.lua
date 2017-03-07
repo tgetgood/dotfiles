a43a05eba15fb8d6e6802e14414d25b862ff7c37
Thomas Getgood <thomas.getgood@gmail.com>
2017-03-07 10:06:34 -0500

    Added back tiling layout.
    Sometimes I don't want the layout to be fair.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -71,7 +71,7 @@ awful.util.tagnames = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.floating,
-  --awful.layout.suit.tile,
+  awful.layout.suit.tile,
   --awful.layout.suit.tile.left,
     awful.layout.suit.fair,
     awful.layout.suit.tile.bottom,
