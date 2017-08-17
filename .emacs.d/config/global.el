a11e250123ec8924c072177d6915292350e90f46
Thomas Getgood <thomas.getgood@gmail.com>
2017-08-17 11:16:41 -0400

    More broken formatting.

--- .emacs.d/config/global.el
+++ .emacs.d/config/global.el
@@ -246,8 +246,8 @@
 	(interactive)
 	(if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
 			(if (< emacs-major-version 22)
-		(save-buffers-kill-terminal)
-	(save-buffers-kill-emacs))
+					(save-buffers-kill-terminal)
+				(save-buffers-kill-emacs))
 		(message "Canceled exit")))
 
 (global-set-key (kbd "C-x C-c") 'ask-before-closing)
