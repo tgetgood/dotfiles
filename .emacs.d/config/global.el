8595778c0422070ee06ec9ff7cdd9b42b4929fbd
Thomas Getgood <thomas.getgood@gmail.com>
2017-08-17 11:13:24 -0400

    Fixed weird indentation

--- .emacs.d/config/global.el
+++ .emacs.d/config/global.el
@@ -57,10 +57,10 @@
 ;;;;;
 ;; Easy window navigation
 ;;;;;
-(global-set-key (kbd "M-h")  'windmove-left)
+(global-set-key (kbd "M-h") 'windmove-left)
 (global-set-key (kbd "M-l") 'windmove-right)
-(global-set-key (kbd "M-k")    'windmove-up)
-(global-set-key (kbd "M-j")  'windmove-down)
+(global-set-key (kbd "M-k") 'windmove-up)
+(global-set-key (kbd "M-j") 'windmove-down)
 
 (global-set-key (kbd "M-H") 'buf-move-left)
 (global-set-key (kbd "M-L") 'buf-move-right)
