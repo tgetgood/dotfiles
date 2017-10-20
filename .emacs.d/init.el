4ee84fe35d4275898a559ad0c983a6ca83a32cf8
Thomas Getgood <thomas.getgood@gmail.com>
2017-10-20 15:31:29 -0400

    Removed uninstallable package.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -76,7 +76,7 @@
 		(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
  '(package-selected-packages
 	 (quote
-		(js2-highlight-vars js2-mode js2-refactor json-mode nodejs-repl xref-js2 latex-extra cider-eval-sexp-fu aggressive-indent 0blayout alchemist elixir-mode dired-single lua-mode exec-path-from-shell buffer-move cider wanderlust tagedit smex scss-mode scion popup meacupla-theme markdown-mode magit kibit-mode kibit-helper jabber ido-ubiquitous haskell-mode gh flycheck-pos-tip evil-smartparens evil-paredit evil-leader cyberpunk-theme company clojure-mode-extra-font-locking cljsbuild-mode clj-refactor bash-completion ag ack-and-a-half)))
+		(js2-highlight-vars js2-mode js2-refactor json-mode nodejs-repl xref-js2 latex-extra aggressive-indent 0blayout alchemist elixir-mode dired-single lua-mode exec-path-from-shell buffer-move cider wanderlust tagedit smex scss-mode scion popup meacupla-theme markdown-mode magit kibit-mode kibit-helper jabber ido-ubiquitous haskell-mode gh flycheck-pos-tip evil-smartparens evil-paredit evil-leader cyberpunk-theme company clojure-mode-extra-font-locking cljsbuild-mode clj-refactor bash-completion ag ack-and-a-half)))
  '(sp-autoskip-closing-pair t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
