e308f3ab64f50e622209d1acc0b190f560499d78
Thomas Getgood <thomas.getgood@gmail.com>
2018-09-06 22:03:39 -0400

    Don't jump focus to errors automatically.

--- .emacs.d/config/clojure-setup.el
+++ .emacs.d/config/clojure-setup.el
@@ -36,6 +36,8 @@
 ;; Wrap when navigating history.
 (setq cider-repl-wrap-history t)
 
+(setq cider-show-error-buffer nil)
+
 ;; Use clojure mode for other extensions
 (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
