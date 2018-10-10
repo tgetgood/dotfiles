94521caf4da6d24bdfb217e8adadb761dfdcf3db
Thomas Getgood <thomas.getgood@gmail.com>
2018-10-10 15:23:51 -0400

    Rust config.

--- .emacs.d/config/rust-setup.el
+++ .emacs.d/config/rust-setup.el
@@ -1,1 +1,23 @@
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
+(add-hook 'rust-mode-hook 'racer-mode)
+(add-hook 'racer-mode-hook 'eldoc-mode)
+(add-hook 'cargo-process-mode-hook 'visual-line-mode)
+
+(setq rust-format-on-save t)
+
+(defun cargo-process-add (crate)
+	"Adds crate to current cargo project."
+	(interactive "sCrate: ")
+	(cargo-process--start "Add" (concat "cargo add " crate)))
+
+(defun cargo-process-install (crate)
+	"Installs the cargo crate in the current toolchain."
+	(interactive "sCrate: ")
+	(cargo-process--start "Install" (concat "cargo install " crate)))
+
+(evil-define-key 'normal racer-mode-map "K" 'racer-describe)
+
+(evil-leader/set-key-for-mode 'rust-mode
+	"c" 'cargo-process-check
+	"b" 'cargo-process-build
+	"r" 'cargo-process-run)
