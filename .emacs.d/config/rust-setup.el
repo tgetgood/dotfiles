bcca91e620b6532264f0e5c5dcf2b330dbd2a0d5
Thomas Getgood <thomas.getgood@gmail.com>
2020-01-02 12:16:22 -0500

    Set rust src path for racer.

--- .emacs.d/config/rust-setup.el
+++ .emacs.d/config/rust-setup.el
@@ -5,6 +5,9 @@
 
 (setq rust-format-on-save t)
 
+(setq racer-rust-src-path
+			"~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
+
 (defun cargo-process-add (crate)
 	"Adds crate to current cargo project."
 	(interactive "sCrate: ")
