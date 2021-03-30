869d5ca886ae623cffce843e2f671096bb725b66
Thomas Getgood <thomas@macroexpanse.com>
2021-03-29 22:13:56 -0400

    Pulled in C stuff from og.

--- .emacs.d/config/clang-setup.el
+++ .emacs.d/config/clang-setup.el
@@ -3,4 +3,17 @@
 (add-hook 'c-mode-hook
           (lambda ()
             (progn
+              (company-mode)
               (semantic-mode 1))))
+
+(add-to-list 'company-backends 'company-c-headers)
+
+(add-to-list 'company-c-headers-path-system "/usr/include/GLFW/")
+(add-to-list 'company-c-headers-path-system "/usr/include/vulkan/")
+
+(evil-leader/set-key-for-mode 'c-mode "c" 'compile)
+(evil-leader/set-key-for-mode 'llvm-mode "c" 'compile)
+
+(load "llvm-mode")
+
+(add-to-list 'auto-mode-alist '("\\.ll\\'" . llvm-mode))
