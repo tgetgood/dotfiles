e3338d56b839dbd18645cfd3ab75ba984924a4b5
Thomas Getgood <thomas@macroexpanse.com>
2021-05-03 10:56:00 -0400

    More js changes.

--- .emacs.d/config/javascript-config.el
+++ .emacs.d/config/javascript-config.el
@@ -20,38 +20,41 @@
 (setq js-indent-level 2)
 
 (add-hook 'js2-mode-hook
-					(add-hook 'before-save-hook 'whitespace-cleanup nil t)
-					(lambda () (setq indent-tabs-mode nil)))
+          (add-hook 'before-save-hook 'whitespace-cleanup nil t)
+          (lambda () (setq indent-tabs-mode nil)))
 
 (defun load-file-in-new-node-repl ()
-	(interactive)
-	(let ((buff (buffer-file-name)))
-		(progn
-			(if (get-buffer "*nodejs*")
-					(progn
-						(nodejs-repl-quit-or-cancel)
-						(nodejs-repl-quit-or-cancel)))
-			(nodejs-repl)
-			(nodejs-repl-load-file buff)
-			(insert "\n")
-			(end-of-buffer))))
+  (interactive)
+  (let ((buff (buffer-file-name)))
+    (progn
+      (if (get-buffer "*nodejs*")
+          (progn
+            (nodejs-repl-quit-or-cancel)
+            (nodejs-repl-quit-or-cancel)))
+      (nodejs-repl)
+      (nodejs-repl-load-file buff)
+      (insert "\n")
+      (end-of-buffer))))
 
 (add-hook 'js2-mode-hook
-					(lambda ()
-						(define-key nodejs-repl-mode-map (kbd "TAB")
-							'nodejs-repl-complete-from-process)
-						(define-key js2-mode-map (kbd "C-c M-j") 'nodejs-repl)
-						(define-key js2-mode-map (kbd "C-c C-k") 'load-file-in-new-node-repl)
-						(define-key js2-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+          (lambda ()
+            (define-key nodejs-repl-mode-map (kbd "TAB")
+              'nodejs-repl-complete-from-process)
+            (define-key js2-mode-map (kbd "C-c M-j") 'nodejs-repl)
+            (define-key js2-mode-map (kbd "C-c C-k") 'load-file-in-new-node-repl)
+            (define-key js2-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
 
 (defun set-eslint-standard ()
-	"Set flycheck to use standard for linting"
-	(interactive)
-	(setq flycheck-javascript-eslint-executable "standard --verbose"))
+  "Set flycheck to use standard for linting"
+  (interactive)
+  (setq flycheck-javascript-eslint-executable "standard --verbose"))
 
 ;;;;; Vue.js
 
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
 
+;;;;; Typescript
+
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
+(setq typescript-indent-level 2)
